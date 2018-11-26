39c74284bbf44987405a21256c6d5df735fb55d6
hristovpln <aboveall@abv.bg>
2018-11-26 16:41:09 +0200

    Update SimpleController.java

--- src/main/java/eu/dreamix/web/SimpleController.java
+++ src/main/java/eu/dreamix/web/SimpleController.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 public class SimpleController {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(SimpleController.class);
-	private final static String VERSION = "3.0.0";
+	private final static String VERSION = "4.0.0";
 
 	private final ApplicationProperties appConfig;
 
