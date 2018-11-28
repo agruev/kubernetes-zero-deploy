b1c20ba3794bedfd429a95c85b566cfaf4619f6d
hristovpln <aboveall@abv.bg>
2018-11-28 12:09:22 +0200

    Update SimpleController.java

--- src/main/java/eu/dreamix/web/SimpleController.java
+++ src/main/java/eu/dreamix/web/SimpleController.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 public class SimpleController {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(SimpleController.class);
-	private final static String VERSION = "3.0.0-RL";
+	private final static String VERSION = "4.0.0-HealthCkeck";
 
 	private final ApplicationProperties appConfig;
 
